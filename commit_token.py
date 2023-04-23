e44a722b88e778ef1682eb70c58565476c9011fc
robertosixty1 <roberto011herminegildo@gmail.com>
2023-04-23 16:27:02 -0300

    Add a script that automatically commit changes made to `random-str-idk-whatis`

---
+++ commit_token.py
@@ -0,0 +1,19 @@
+#!/bin/env python3
+
+from subprocess import run
+from sys import stderr
+
+def get_password():
+    try:
+        f = open("./.sshpasswd", "r")
+        return f.read().strip()
+    except FileNotFoundError:
+        print("ERROR: Could not find file `.sshpasswd`", file=stderr)
+        exit(1)
+
+cmd = run(["git", "status"], capture_output=True, text=True)
+
+if not ("nothing to commit" in cmd.stdout):
+    run(["git", "add", "random-str-idk-whatis"])
+    run(["git", "commit", "-m", "Update token-like-thingy"])
+    run(["git", "push", "origin", "master"], input=get_password().encode())
